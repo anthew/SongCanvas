ed64deb684c039c52e5827dfa5ace901278bd19e
Anthony Hewitt <hewit017@cougars.csusm.edu>
2023-10-11 18:10:40 -0700

    Update Nodejs-on-ec2.js

--- Nodejs-on-ec2.js
+++ Nodejs-on-ec2.js
@@ -5,3 +5,6 @@ app.use(morgan(":method :url :status :res[content-length] - :response-time ms"))
 app.get("/test", (req, res) => {
   res.send("<h1>It's working 🤗</h1>")
 })
+
+const port = process.env.PORT || 8080
+app.listen(port, () => console.log(`Listening on port ${port}`))
