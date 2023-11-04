98956259ad0b911e612a60b3909c1832959a279a
anthew <hewit017@cougars.csusm.edu>
2023-11-04 10:17:14 -0700

    changed folder from "public" to "website" and added error message.

--- server.js
+++ server.js
@@ -3,10 +3,17 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'website')));
 app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, 'login.html'));
 });
+
+app.use((req, res) => {
+    res.status(404) //error message
+    res.send(`
+  <h1>Error message</h1>
+  <img src="adassadasdas">
+  `)})
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, _ => {
