a910ee3a190fc9b103b608c9c2ef717b58fc4c79
nagalil-honaj-hteal3 <elaeth.jonah@gmail.com>
2023-12-13 10:10:22 -0800

    my changes

--- js/login/main.js
+++ js/login/main.js
@@ -16,32 +16,3 @@ function initializePasswordInputToggle() {
     new PasswordInputToggle(eyeButton, passwordInput);
   }
 }
-
-
-// function validateCredentials(event) {
-//   // Prevent default form submission behavior
-//   console.log("Validate Credentials function called")
-
-//   event.preventDefault();
-
-//   const emailInput = document.getElementById("email-input").value;
-//   const passwordInput = document.getElementById("password-input").value;
-
-//   // Call the checkCredentials function to validate against the database
-//   UserDB.checkCredentials(emailInput, passwordInput)
-//      .then((result) => {
-//         if (result) {
-//            // Credentials are valid
-//            console.log('Credentials are valid');
-//            // Manually submit the form
-//            document.getElementById("login-form").submit();
-//         } else {
-//            // Credentials are invalid
-//            console.log('Credentials are invalid');
-//            // Add your logic for displaying an error message or other actions here
-//         }
-//      })
-//      .catch((error) => {
-//         console.error('Error checking credentials:', error.message);
-//      });
-// }
