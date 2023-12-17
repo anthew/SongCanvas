01344d2018da566a2d4c34789c8a9ccbb4b51e76
Soulpuncher1234 <4ahmadrahman@gmail.com>
2023-12-16 22:17:11 -0800

    Add the Konva Canvas stuff :)

--- website/js/editor/editor.js
+++ website/js/editor/editor.js
@@ -1,8 +1,18 @@
+// const konva = document.getElementById("KonvaCanvas");
+
+// let kwidth = this.konva.style.width = 650;
+// let kheight = this.konva.style.height = 500;
+
+
 var stage = new Konva.Stage({
     container: 'KonvaCanvas',
-    width: 650,
-    height: 500,
+    // width: 650,  
+    // height: 500, 
 });
+
+// can i replace whats in the parenthesis with this i higlight
+stage.width(600);
+stage.height(500);
 
 var layer = new Konva.Layer(); 
 
