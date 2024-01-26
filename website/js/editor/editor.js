61f9103541827165565b1f1432fed41936feb1b3
Soulpuncher1234 <4ahmadrahman@gmail.com>
2024-01-26 14:16:46 -0800

    Add the function to where shapes can be displayed and hidden at specified times

--- website/js/editor/editor.js
+++ website/js/editor/editor.js
@@ -7,26 +7,31 @@ var stage = new Konva.Stage({
 
 
 //Create and add layer to the stage
-var layer = new Konva.Layer(); 
+var layer = new Konva.Layer();
 stage.add(layer);
 
-//Test if the canvas is working
-var shapes = new Konva.Rect({
-    x: 0,
-    y: 0,
-    width: 100,
-    height: 50,
-    fill: 'green',
-    stroke: 'black',
-    strokeWidth: 1,
-    name: "shape1",
-});
+//Testing with animation 
+// var anim = new Konva.Animation(function (frame) {
+//     var angleDiff = (frame.timeDiff * 90) / 1000;
+//     shapes.rotate(angleDiff);
+//   }, layer);
 
-layer.add(shapes);
+// anim.start();
 
 //Function deticated to creating shapes based on user input
 var addShapeToScreenButton = document.getElementById("shapeSubmit");
 addShapeToScreenButton.addEventListener("click", createRectangle);
+
+//Array to hold shapes user has added 
+let ShapeArray = []; //Holds the information of a shape and it's start time, end time, and animation
+let ShapeArrayIndex = 0;
+
+let ShapeStartArray = []; //Used to keep track of the shapes from earliest start time to latest start time
+let ShapeStartIndex = 0;
+
+let ShapeEndArray = []; //Used to keep track of the shapes from earliest end time to the latest end time
+let ShapeEndIndex = 0;
+
 
 function createRectangle()
 {
@@ -38,8 +43,11 @@ function createRectangle()
     let strokeWidth = document.getElementById("strokeWidth").value;
     let x = document.getElementById("x").value;
     let y = document.getElementById("y").value;
-     
-    alert("Shape added");
+
+    //Testing
+    let startTime = document.getElementById("start-time").value;
+    let endTime = document.getElementById("end-time").value;
+    
     var rect = new Konva.Rect({
         name: name,
         width: width,
@@ -49,10 +57,36 @@ function createRectangle()
         strokeWidth: strokeWidth,
         x: x,
         y: y,
-    });
+        visible: false,
+    });    
 
     //Add the newly created shape to the canvas
     layer.add(rect);
+
+    //Add the newly created shape to the array
+    var newShape = {
+        "startTime" : startTime,
+        "endTime" : endTime,
+        "shape" : rect,
+    }
+
+    //Save the shape to array that will be used to store to database 
+    ShapeArray.push(newShape);
+
+    //Add the shape to an array that sorted by start time
+    ShapeStartArray.push(newShape);
+    ShapeStartArray.sort(function (a, b) {
+        return a.startTime.localeCompare(b.startTime);
+    });
+
+    //Add the shape to an array that is sorted by end time
+    ShapeEndArray.push(newShape);
+    ShapeEndArray.sort(function (a, b) {
+        return a.endTime.localeCompare(b.endTime);
+    });
+
+    //Inidacte to user that the shape was added
+    alert("Shape created");
 }
 
 //TextLayer
@@ -78,10 +112,12 @@ let backgroundArray = [
         "contentFile" : "/Files/CarrieUnderwood.mp4"
     },
     {
-        "startTime" : "00:54.27",
+        "startTime" : "00:25.27",
         "contentFile" : "/Files/nature.jpg"
     },
 ]
+
+
 
 let backgroundIndex = 0; //Keeps track of the current background were going to use
 var imageCont = document.getElementById('imageContent'); //Used to acces the image element and manipulate it
@@ -137,6 +173,7 @@ document.addEventListener('keydown', function(event){
 
             break;
 
+        //If any other button do nothing
         default:
             return;
     }
@@ -144,7 +181,8 @@ document.addEventListener('keydown', function(event){
 
 //Function to display elements at specific times. Called by setInterval
 function updateProjectElements(formattedTime){
-    console.log(formattedTime);
+    //console.log(formattedTime);
+
     //Round the time to the lowest integer
     //var audioTimeStamp = Math.floor(audioElement.currentTime);
 
@@ -174,9 +212,37 @@ function updateProjectElements(formattedTime){
             imageCont.src=backgroundArray[backgroundIndex].contentFile;
         }
         
+        //Increment background index if current index is not at the end of array
         if(backgroundIndex < backgroundArray.length-1)
             backgroundIndex+=1;   
     } 
+
+
+    /******************Manage shapes******************/ /*Note to self might need to work on formatted time. Program to slow to make changes in time*/
+    if(ShapeStartArray.length!=0 && ShapeStartArray[ShapeStartIndex].startTime==formattedTime) //Display shape when it's start time meets formattedTime
+    {
+        //Display the shape
+        ShapeStartArray[ShapeStartIndex].shape.show();
+
+        //Check if there is any animations for this shape
+        //ShapeStartArray[ShapeStartIndex].animation.start();
+
+        //Move to the next shape wating to be displayed. Check if we had exceeded the array boundry
+        if(ShapeStartIndex < ShapeArray.length-1)
+            ShapeStartIndex+=1;
+    }
+
+    if(ShapeArray.length!=0 && ShapeEndArray[ShapeEndIndex].endTime==formattedTime)
+    {
+        //Hide the shape
+        ShapeEndArray[ShapeEndIndex].shape.hide();
+
+        //Stop the shapes animation if applicable
+
+        //Move to the next shape if it is available
+        if(ShapeEndIndex < ShapeArray.length-1)
+            ShapeEndIndex+=1;
+    }
 
     /*
     //Add, modify, delete design elements
