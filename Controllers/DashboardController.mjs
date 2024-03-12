7a6713781625f282667b3e6ae7b552b29cd91e3f
nagalil-honaj-hteal3 <elaeth.jonah@gmail.com>
2024-03-11 21:17:56 -0700

    added changes for dashboard.html and controller for creating projects table

--- Controllers/DashboardController.mjs
+++ Controllers/DashboardController.mjs
@@ -1,4 +1,3 @@
-
 $(document).ready(function(){
     $("#editorModal").click(populateEditorTable);
 
@@ -16,3 +15,36 @@ $(document).ready(function(){
         })
     }
 });
+
+var addProject = document.getElementById("projectSubmit");
+addProject.addEventListener("click", requestAddProject);
+
+var projectArray = [];
+
+function requestAddProject() {
+    let projectName = document.getElementById("insertProjectName").value;
+    let audioFile = document.getElementById("audioInput").value;
+
+    let projectTable = document.getElementById("editorProjects");
+
+    let template = `
+            <tr colspan="4">
+                <td id="editShape " style="border: 1px solid black;">
+                    <button style="background-color: white; border: none;">${projectName}</button>
+                </td>
+                <td id="addedShapeVisible" style="border: 1px solid black;">
+                    <button style="background-color: white; border: none;"><img style="width: 26px;
+                    height: 26px;" src='/LoginMedia/EyeShow.png' ></button>
+                </td>
+                <td id="deleteShape" style="border: 1px solid black;">
+                    <button style="background-color: white; border: none;"><img src='/EditorMedia/TrashCan.png' style="width: 26px;
+                    height: 26px; "></button>
+                </td>
+                <td >
+                    <input type="checkbox" style="margin: auto; width: 100%; height: 100%;">
+                </td>
+            </tr>
+        `;
+    
+    projectTable.innerHTML += template;
+}
